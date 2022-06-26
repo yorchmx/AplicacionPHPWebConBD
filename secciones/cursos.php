2c438863f267bace961b2db70f7702fb9d212a67
Jorge Manrique <al02712191@tecmilenio.mx>
2022-06-26 01:32:57 -0500

    cursos php y vista cursos

--- secciones/cursos.php
+++ secciones/cursos.php
@@ -4,4 +4,24 @@
 include_once '../configuraciones/db.php';
 $conexionBD=BD::crearInstancia();
 
+$id=isset($_POST['id'])?$_POST['id']:'';
+$nombre_clase=isset($_POST['nombre_curso'])?$_POST['nombre_curso']:'';
+$accion=isset($_POST['accion'])?$_POST['accion']:'';
+
+if($accion!=''){
+
+    switch($accion){
+
+        case 'agregar':
+             $sql="INSERT INTO cursos (id, nombre_clase) VALUES (NULL, '$nombre_clase')";
+             echo $sql;
+        break;
+    }    
+}
+
+$consulta=$conexionBD->prepare("SELECT * FROM cursos");
+$consulta->execute();
+$listaCursos=$consulta->fetchAll();
+
+
 ?>
--- secciones/vista_cursos.php
+++ secciones/vista_cursos.php
@@ -1,46 +1,43 @@
 <?php include('../templates/cabecera.php'); ?>
 <?php include('../secciones/cursos.php'); ?>
 
+<div class="row">
+    <div class="col-12">
+        <br/>
+        <div class="row">
+        <div class="col-md-5">
+        <form action="" method="post">
+        <div class="card">
+            <div class="card-header"> Cursos </div>
+            <div class="card-body">
+            <div class="mb-3">
+                <label for="" class="form-label">ID</label>
+                <input type="text"
+                    class="form-control" 
+                    name="id" 
+                    id="id" 
+                    aria-describedby="helpId" placeholder="ID">
+            </div>
+            <div class="mb-3">
+                <label for="nombre_curso" class="form-label">Nombre</label>
+                <input type="text"
+                        class="form-control" 
+                        name="nombre_curso" 
+                        id="nombre_curso" aria-describedby="helpId" 
+                        placeholder="Nombre del curso">
+            </div> 
 
+            <div class="btn-group" role="group" aria-label="">
+                <button type="submit" name="accion" value="agregar" class="btn btn-success">Agregar</button>
+                <button type="submit" name="accion" value="editar" class="btn btn-warning">Editar</button>
+                <button type="submit" name="accion" value="borrar" class="btn btn-danger">Borrar</button>
+            </div>
 
-<div class="col-md-5">
+        </div>
 
-<form action="" method="post">
-
-<div class="card">
-    <div class="card-header">
-        Cursos
-    </div>
-    <div class="card-body">
-
-    <div class="mb-3">
-  <label for="" class="form-label">ID</label>
-  <input type="text"
-    class="form-control" 
-    name="id" 
-    id="id" 
-    aria-describedby="helpId" placeholder="ID">
-</div>
-<div class="mb-3">
-  <label for="nombre_curso" class="form-label">Nombre del curso</label>
-  <input type="text"
-    class="form-control" name="nombre_curso" id="nombre_curso" aria-describedby="helpId" placeholder="">
-</div> 
-    <div class="btn-group" role="group" aria-label="">
-        <button type="button" class="btn btn-success">Agregar</button>
-        <button type="button" class="btn btn-warning">Editar</button>
-        <button type="button" class="btn btn-danger">Borrar</button>
-    </div>
-
-    </div>
-   
-</div>
+   </div>
 
 </form>
-
-
-
-
 
 </div>
 <div class="col-md-7">
@@ -54,11 +51,17 @@
         </tr>
     </thead>
     <tbody>
+
+    <?php foreach($listaCursos as $curso){?>
         <tr>
-            <td> 1 </td>
-            <td> Tenis </td>
+            <td> <?php echo $curso['id']; ?> </td>
+            <td> <?php echo $curso['nombre_clase']; ?> </td>
             <td> Seleccionar </td>
         </tr>
+    <?php } ?>
+
+
+
     </tbody>
 </table>
 
